a829030e2f012e193224243547f28e55bf30af94
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2024-02-13 23:22:12 +0300

    feat: configure lsp-lens, nvim-chainsaw

---
+++ nvim/user/plugins/lsp-lens-nvim/init.lua
@@ -0,0 +1,7 @@
+return {
+  "VidocqH/lsp-lens.nvim",
+  event = "User AstroLspSetup",
+  opts = {
+    enable = false,
+  },
+}
---
+++ nvim/user/plugins/nvim-chainsaw/init.lua
@@ -0,0 +1,53 @@
+local prefix = "<leader>L"
+local maps = { n = {} }
+local icon = vim.g.icons_enabled and " " or ""
+maps.n[prefix] = { desc = icon .. "Log" }
+require("astronvim.utils").set_mappings(maps)
+
+return {
+  "chrisgrieser/nvim-chainsaw",
+  event = "User AstroFile",
+  opts = {},
+  keys = {
+    {
+      prefix .. "v",
+      function() require("chainsaw").variableLog() end,
+      desc = "Variable log",
+    },
+    {
+      prefix .. "o",
+      function() require("chainsaw").objectLog() end,
+      desc = "Object log",
+    },
+    {
+      prefix .. "a",
+      function() require("chainsaw").assertLog() end,
+      desc = "Assert log",
+    },
+    {
+      prefix .. "m",
+      function() require("chainsaw").messageLog() end,
+      desc = "Message log",
+    },
+    {
+      prefix .. "b",
+      function() require("chainsaw").beepLog() end,
+      desc = "Beep log",
+    },
+    {
+      prefix .. "t",
+      function() require("chainsaw").timeLog() end,
+      desc = "Time log",
+    },
+    {
+      prefix .. "d",
+      function() require("chainsaw").debugLog() end,
+      desc = "Debug log",
+    },
+    {
+      prefix .. "r",
+      function() require("chainsaw").removeLogs() end,
+      desc = "Remove all log statements",
+    },
+  },
+}
