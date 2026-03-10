ca3a9c0394aaa14fbb8c8593cb19fefc57913b02
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2026-03-10 21:51:56 +0300

    feat(nvim): configure snacks.input

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -3,6 +3,26 @@ return {
   {
     "folke/snacks.nvim",
     opts = function(_, opts)
+      opts.input = vim.tbl_deep_extend("force", opts.input or {}, {
+        icon = " ",
+        prompt_pos = "title",
+        expand = true,
+        win = {
+          style = "input",
+        },
+      })
+
+      opts.styles = opts.styles or {}
+      opts.styles.input = vim.tbl_deep_extend("force", opts.styles.input or {}, {
+        backdrop = 30,
+        relative = "cursor",
+        row = -3,
+        col = 0,
+        width = 50,
+        border = "rounded",
+        title_pos = "center",
+      })
+
       opts.dashboard = opts.dashboard or {}
       opts.dashboard.preset = opts.dashboard.preset or {}
 
